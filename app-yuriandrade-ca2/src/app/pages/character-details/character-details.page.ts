e8ae9b7354978a811b00767d2348af19ba095bbb
Yuri Andrade <yurisfa@gmail.com>
2020-04-14 19:05:09 +0000

    consume some API characters

--- app-yuriandrade-ca2/src/app/pages/character-details/character-details.page.ts
+++ app-yuriandrade-ca2/src/app/pages/character-details/character-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //Add the ActivatedRoute, where can be used further in our code to path/navigate 
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-character-details',
@@ -9,14 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CharacterDetailsPage implements OnInit {
 
-    characterID = null;
+    character: any;
+    //characterID = null;
     //First of all we set the character ID, then we can use the constructor 
 
-    constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(private activatedRoute: ActivatedRoute, private http:HttpClient) { }
 
     ngOnInit() {
-        //Here the function use the path from constructor to GET paraments pased in ID.
-        this.characterID = this.activatedRoute.snapshot.paramMap.get('id');
+        //Old - Here the function use the path from constructor to GET paraments pased in ID.
+        //Old - this.characterID = this.activatedRoute.snapshot.paramMap.get('id');
+        let id = this.activatedRoute.snapshot.paramMap.get('id');
+        this.http.get('https://www.breakingbadapi.com/api/characters/${id}').subscribe( res => {
+            this.character = res;
+        })
+
     }
 
 }
--- app-yuriandrade-ca2/src/app/pages/characters/characters.page.ts
+++ app-yuriandrade-ca2/src/app/pages/characters/characters.page.ts
@@ -4,26 +4,45 @@ import { Router } from '@angular/router';
 // We had to import the NavController and Router
 //NavController helps to navigate inside the app, commmonly used 
 //Or we set the root direct using Router
+import { Observable } from 'rxjs';
+//The observable will be able to receive multiple return values from HTTP requests 
+import { HttpClient } from '@angular/common/http';
+//import this will allow to make request to HTTP! 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.page.html',
-  styleUrls: ['./characters.page.scss'],
+    selector: 'app-characters',
+    templateUrl: './characters.page.html',
+    styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
 
-  constructor(private navController: NavController, private router: Router) { }
-//What is a contructor? We use the constructor to storage services and classes 
-//Once storaged,can be used using .this ammend. 
-//This function created shows 2 paths to navigate 
-  ngOnInit() {
-  }
+    characters: Observable<any>;
 
-  openDetails(){
-      //these function was created in characters.page.html 
-      //this will set the logic to call the function with correct router
-      //We see .this beeing used in this method to use the function from constructor
-      this.router.navigateByUrl('/tabs/characters/1');
-  }
-  //We are not using NavController, yet! 
+    constructor(private navController: NavController, private router: Router, private http: HttpClient) { }
+    //What is a contructor? We use the constructor to storage services and classes 
+    //Once storaged,can be used using .this ammend. 
+    //This function created shows 2 paths to navigate 
+
+    ngOnInit() {
+        //Now we will make connection when the method is called in the view!
+        //characters variable will be able to get data from API with GET protocols
+
+        this.characters = this.http.get('https://www.breakingbadapi.com/api/characters');
+        this.characters.subscribe(data => {
+            console.log('my data: ', data);
+        })
+    }
+
+    openDetails(character) {
+        
+        //these function was created in characters.page.html 
+        //this will set the logic to call the function with correct router
+        //We see .this beeing used in this method to use the function from constructor
+        //this.router.navigateByUrl('/tabs/characters/${character}');
+        //If we set an ID, we can have the following code
+        //let characterID = character.url.split('/');
+       // let characterID = split[split.length-2]; 
+        this.router.navigateByUrl(`/tabs/characters/${character}`);  
+    }
+    //We are not using NavController, yet! 
 
 }
